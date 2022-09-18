5677d1ab56221c7317c5e89cbea4a898a95e91e2
jc-nieto <jc.nieto@uniandes.edu.co>
2022-09-17 19:04:52 -0500

    se crea servicio para club

--- src/club/club.module.ts
+++ src/club/club.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClubEntity } from './club.entity';
+import { ClubService } from './club.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ClubEntity])],
-  providers: [],
+  providers: [ClubService],
 })
 export class ClubModule {}
