661ade58f7eb3983dd9f08448b65130a598f686f
jc-nieto <jc.nieto@uniandes.edu.co>
2022-09-17 20:11:48 -0500

    se crea servicio para member

--- src/member/member.module.ts
+++ src/member/member.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MemberService } from './member.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([MemberModule])],
-  providers: [],
+  providers: [MemberService],
 })
 export class MemberModule {}
